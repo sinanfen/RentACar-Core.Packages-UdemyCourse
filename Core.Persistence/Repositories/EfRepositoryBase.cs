fe886ffe325d1bd27af2f7b2b4011f380a2ca4d5
Sinan <sinanfen@hotmail.com>
2023-08-26 23:41:44 +0300

    refactor

--- Core.Persistence/Repositories/EfRepositoryBase.cs
+++ Core.Persistence/Repositories/EfRepositoryBase.cs
@@ -3,14 +3,9 @@ using Core.Persistence.Paging;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Query;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Persistence.Repositories;
 
