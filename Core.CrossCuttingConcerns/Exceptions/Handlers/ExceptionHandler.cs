f4905be77a81462db233ad9bbf1144aca26eeeeb
Sinan <sinanfen@hotmail.com>
2023-09-13 05:00:18 +0300

    exceptionMiddlewareExtensions added. Advanced Global Exception Handling applied

---
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -0,0 +1,21 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
+
+public abstract class ExceptionHandler
+{
+    public Task HandleExceptionAsync(Exception exception) =>
+        exception switch
+        {
+            BusinessException businessException => HandleException(businessException),
+            _ => HandleException(exception) //exception hiçbiri değilse yani default olarak bunu bas
+        };
+
+    protected abstract Task HandleException(BusinessException businessException);
+    protected abstract Task HandleException(Exception exception);
+}
